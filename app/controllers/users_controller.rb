b877623e18cb393066652dc3eb2255c63984ea01
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-01 14:18:59 +0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,17 @@ class UsersController < ApplicationController
 
   def create
     @user=User.create(user_params)
-    if @user.password_confirmation != params[:user][:password]
-      # !@user.authenticate(params[:user][:password])
-      redirect_to new_user_path
-    else
+    if @user.save
       session[:user_id]=@user.id
+    else
+      redirect_to new_user_path
     end
+    # if @user.password_confirmation == params[:user][:password]
+    #   # @user.authenticate(params[:user][:password])
+    #   session[:user_id]=@user.id
+    # else
+    #   redirect_to new_user_path
+    # end
   end
 
   def new
