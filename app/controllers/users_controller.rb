d9244bc5ba200466c56eb9b54681ff14a4abdeac
github username <github email address>
2019-08-03 19:04:44 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def create
+        binding.pry
         @user = User.create(user_params)
         session[:user_id] = @user.id
         if !@user.authenticate(params[:password])
